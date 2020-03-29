4dae3483b8afc0c36730dfe3e8c1eee635a39972
André Cirne <ancirne@gmail.com>
2020-03-29 16:35:23 +0100

    Fix automated builds

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker build -t cirne/openvas-light \
-    --build-arg OPENVAS_VERSION=7.0.0 \
-    --build-arg VERSION=1.0 \
-    --build-arg GVM_LIBS_VERSION=11.0.0 \
-    --build-arg GOCROND_VERSION=0.6.1 .
-    --build-arg ARCH=64
